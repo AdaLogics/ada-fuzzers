a3c28ce4ea4c2c6eecfa86255f76ac50269f18a3
David Korczynski <david@adalogics.com>
2023-12-25 08:38:03 -0800

    llvm: fix build
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/llvm/copy-over.sh
+++ projects/llvm/copy-over.sh
@@ -1,7 +1,8 @@
+#!/bin/bash -eux
 # This assumes ada-fuzzers is located under $SRC/ada-fuzzers and will copy
 # over all fuzzers into mainstream llvm at $SRC/llvm-project
 
-ADA_SRC=$SRC/ada-fuzzers/project/llvm/
+ADA_SRC=$SRC/ada-fuzzers/projects/llvm/
 LLVM_DST=$SRC/llvm-project
 
 cp -rf $ADA_SRC/llvm-parse-assembly-fuzzer $LLVM_DST/llvm/tools/llvm-parse-assembly-fuzzer
